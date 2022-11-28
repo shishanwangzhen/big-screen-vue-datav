b81646cf1a2e3579b75824e40ac56a7cd77daed8
qin-xiaoting1 <361602174@qq.com>
2022-11-28 09:34:23 +0800

    1、采集器接口调用 2、解码器接口调用 3、md文件修改编辑

--- src/bus.js
+++
@@ -1,5 +0,0 @@
-// bus.js文件内容
-import Vue from 'vue'
-// export default new Vue()
-const Bus = new Vue()
-export default Bus
--- src/mock/mockServer.js
+++
@@ -1,7 +0,0 @@
-// 引入mock 模块
-// import Mock from 'mockjs'
-// 引入json数据，json数据是默认暴露的，可以直接引入
-
-// import roleTabl from './roleTable.json'
-
-// Mock.mock('./mock/roleTabl',{code:200,data:roleTabl})
